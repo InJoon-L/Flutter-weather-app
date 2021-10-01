de64b0bbe05c72e87aa8273511348cd69f87f4f1
InJoon-L <dlswns619@naver.com>
2021-10-01 15:38:25 +0900

    http와 dart.convert를 이용해서 json데이터를 복호화

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({ Key? key }) : super(key: key);
@@ -14,18 +16,39 @@ class _LoadingState extends State<Loading> {
   void initState() {
     super.initState();
     getLocation();
+    fetchData();
   }
   
   void getLocation() async{
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    print(position);
+    try {
+      Position position = await Geolocator.
+      getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      print(position);
+    } catch (e) {
+      print('There was a problem with the internet connection.');
+    }
+  }
+
+  void fetchData() async{
+    http.Response res = await http.get(Uri.parse('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
+    if (res.statusCode == 200) {
+      String jsonData = res.body;
+      var myJson = jsonDecode(jsonData)['weather'][0]['description'];
+      print(myJson);
+
+      var wind = jsonDecode(jsonData)['wind']['speed'];
+      print(wind);
+
+      var id = jsonDecode(jsonData)['id'];
+      print(id);
+    }
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: null, 
+          onPressed: null,
           child: Text(
             'Get my location',
             style: TextStyle(
